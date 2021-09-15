1efa83336ad38aaa50b90ec1908575246b4aef71
PAULO HENRIQUE DE ANDRADE JUNIOR <21193420@CFP127ALUNO.LOCAL>
2021-09-15 14:47:41 -0300

    Inicio do Projeto

---
+++ bd/conexaoMysql.php
@@ -0,0 +1,14 @@
+<?php
+/*******************************************************
+    Objetivo: Arquivo para configurar a conexão com o 
+    banco de dados MySql
+    Data: 15/09/2021
+    Responsável: Paulo Henrique
+********************************************************/
+function conexaoMysql(){
+    $server = (string) null;
+    $user = (string) null;
+    $password = (string) null;
+    $database = (string) null;
+}
+?>
